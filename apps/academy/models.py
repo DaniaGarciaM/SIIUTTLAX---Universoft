205eb5242c030b6368b223c94f8d7269fa0098e6
Dania Garcia <daniagm75@gmail.com>
2024-06-21 20:15:07 -0600

    Corrección de satus

--- apps/academy/models.py
+++ apps/academy/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Employee(User):
+    tipos = [
+        ('prof','Docente'),
+        ('direc','Director')
+    ]
+    num_empleado = models.IntegerField()
+    tipo = models.CharField(max_length=15, choices=tipos)
+
+    def __str__(self):
+        return self.num_empleado
+    
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+    
+class Student(User):
+    matricula = models.CharField(max_length=20)
+    def __str__(self):
+        return self.matricula
+    
+    class Meta: #Español
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
+    
