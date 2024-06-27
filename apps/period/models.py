dfd4ad5ea54dca65fae421e2b29e0d1f35e62434
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-06-27 12:59:25 -0600

    Creacion de period JJRM

---
+++ apps/period/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Period(models.Model):
+    PERIODS = [
+        ('enero - abril', 'enero - abril'),
+        ('mayo - agosto', 'mayo - agosto'),
+        ('septiembre - diciembre', 'septiembre - diciembre')
+    ]
+    period = models.CharField(max_length=25, choices=PERIODS)
+    year = models.CharField(max_length=4)
+    cicle = models.CharField(max_length=11, default='2023-2024')
+
+class semester(models.Model):
+    semester = models.IntegerField()
+    semester_name = models.CharField(max_length=20)
