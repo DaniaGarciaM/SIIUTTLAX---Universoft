8b9dbc5c6fb1f9c3648abbe8997754bf40ed5429
Dania Garcia <daniagm75@gmail.com>
2024-06-27 13:07:04 -0600

    Correción model academy -DGM

--- apps/academy/admin.py
+++ apps/academy/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student,Employee
+from .models import Student,Category,Professor
 
 admin.site.register(Student)
-admin.site.register(Employee)
+admin.site.register(Category)
+admin.site.register(Professor)
