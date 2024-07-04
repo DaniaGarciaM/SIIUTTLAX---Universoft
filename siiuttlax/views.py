1aa86bb995f9ff1c0bde61c8b47a3cc205808041
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-07-04 13:27:28 -0600

    aplicando validacion de login - MASP

--- siiuttlax/views.py
+++ siiuttlax/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 
 def usuarios_view(request):
     return render(request, 'users/usuarios.html')
