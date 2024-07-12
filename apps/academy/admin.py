e9d9bd0fc2178b6f0076fea0881f2b45eb80d7d7
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-07-12 11:44:53 -0600

    Corrigiendo admin de category -JJRM

--- apps/academy/admin.py
+++ apps/academy/admin.py
@@ -6,8 +6,8 @@ from .models import Student,Category,Professor
 admin.site.register(Student)
 @admin.register(Professor)
 class ProfessorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'category', 'no_empleado', 'title')
-    fields = ('first_name', 'last_name', 'category', 'no_empleado', 'title')
+    list_display = ('first_name', 'last_name', 'category', 'employee_number', 'title')
+    fields = ('first_name', 'last_name', 'category', 'employee_number', 'title')
 admin.register(Student)
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'enrollment')
