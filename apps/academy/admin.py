033ab3b59f7b354340c4d9401ef7f7911a040857
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-07-12 11:56:50 -0600

    Corrigiendo admin de academy -JJRM

--- apps/academy/admin.py
+++ apps/academy/admin.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Student,Category,Professor
 
-admin.site.register(Student)
+admin.site.register(Category)
 @admin.register(Professor)
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'category', 'employee_number', 'title')
     fields = ('first_name', 'last_name', 'category', 'employee_number', 'title')
-admin.register(Student)
+@admin.register(Student)
 class ProfessorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'enrollment')
-    fields = ('first_name', 'last_name', 'enrollment', 'email')
+    list_display = ('first_name', 'last_name', 'matricula')
+    fields = ('first_name', 'last_name', 'matricula', 'email')
+
+
