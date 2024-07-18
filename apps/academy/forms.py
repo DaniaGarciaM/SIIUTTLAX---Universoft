a06fe0c3c3632457d7df3f01f3a34142aafbc017
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-07-18 13:38:47 -0600

    Forms de academy -JJRM

--- apps/academy/forms.py
+++ apps/academy/forms.py
@@ -5,10 +5,11 @@ class ProfessorForm(forms.ModelForm):
 
     class Meta:
         model = Professor
-        fields= ['username', 'password', 'first_name', 'employee_number']
+        fields= ['username', 'password', 'first_name', 'employee_number', 'category']
         widgets = {
             'password': forms.PasswordInput(),
-            'employee_number': forms.NumberInput()
+            'employee_number': forms.NumberInput(),
+            'category': forms.Select()
         }
 
 class StudentForm(forms.ModelForm):
