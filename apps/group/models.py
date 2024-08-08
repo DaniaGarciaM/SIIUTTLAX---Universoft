de23bccd66f16973591e1c99f2b0fb98a24255f6
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-08-08 12:48:02 -0600

    Arreglando el modelo Group -JJRM

--- apps/group/models.py
+++ apps/group/models.py
@@ -7,7 +7,7 @@ from apps.career.models import Subject
 from apps.academy.models import Student
 
 class Group (models.Model):
-    semester_id = models.ForeignKey(
+    semester = models.ForeignKey(
         Semester,
         on_delete=models.CASCADE,
         null=True,
@@ -16,27 +16,33 @@ class Group (models.Model):
     )
     group = models.CharField(max_length=1, verbose_name="Grupo")
     observations = models.CharField(max_length=100, verbose_name="Observaciones")
-    period_id = models.ForeignKey(
+    period = models.ForeignKey(
         Period,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         verbose_name="Periodo"
     )
-    career_id = models.ForeignKey(
+    career = models.ForeignKey(
         Career,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         verbose_name="Carrera"
     )
-    tutor_id = models.ForeignKey(
+    tutor = models.ForeignKey(
         Professor,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         verbose_name="Tutor"
     )
+    students = models.ManyToManyField(
+        Student,
+        verbose_name="Student"
+        
+    )
+    
 
 class Group_Subject(models.Model):
     group_id = models.ForeignKey(
@@ -46,14 +52,14 @@ class Group_Subject(models.Model):
         blank=True,
         verbose_name="Grupo"
     )
-    subject_id = models.ForeignKey(
+    subject = models.ForeignKey(
         Subject,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         verbose_name="Materia"
     )
-    professor_id = models.ForeignKey(
+    professor = models.ForeignKey(
         Professor,
         on_delete=models.CASCADE,
         null=True,
@@ -62,14 +68,14 @@ class Group_Subject(models.Model):
     )
 
 class Group_Student(models.Model):
-    group_id = models.ForeignKey(
+    group= models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         verbose_name="Grupo"
     )
-    student_id = models.ForeignKey(
+    student = models.ForeignKey(
         Student,
         on_delete=models.CASCADE,
         null=True,
