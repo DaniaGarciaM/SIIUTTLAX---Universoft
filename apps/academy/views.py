9409b2c3204a38b66f433f5236b17bc192e13e8b
Leonel David Navarro Meneses <leoneldavidnavarromeneses66@gmail.com>
2024-08-12 13:37:38 -0600

    edicion de views - LDNM

--- apps/academy/views.py
+++ apps/academy/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import redirect, render
 from .forms import ProfessorForm, StudentForm
 from .models import Professor, Student
 from django.contrib import messages
-# Create your views here.
+
+
 def create_professor(request):
     if request.method == 'POST':
         form = ProfessorForm(request.POST)
@@ -11,10 +12,12 @@ def create_professor(request):
             form.save()
             return redirect('academy/create_professor')
         
+
     form = ProfessorForm() 
     return render(request,
                   'academy/create_professor.html',
                   {'form': form})
+
 
 def create_student(request):
     if request.method == 'POST':
@@ -22,6 +25,7 @@ def create_student(request):
         if form.is_valid():
            form.save()
            return redirect('academy/create_student')
+        
         
     form = ProfessorForm()    
     form = StudentForm()
